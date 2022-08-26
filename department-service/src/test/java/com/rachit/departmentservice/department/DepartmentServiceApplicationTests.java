dba5fc417279d773f0e3d7a77e645d1cf03c92a1
sharmrac <rachit.sharma@hp.com>
2022-08-26 21:41:15 +0530

    Initial Setup for Adding and Fetching Department

---
+++ department-service/src/test/java/com/rachit/departmentservice/department/DepartmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rachit.departmentservice.department;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
