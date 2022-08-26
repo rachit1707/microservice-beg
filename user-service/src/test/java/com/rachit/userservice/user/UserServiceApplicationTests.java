5945299a32791acb49de4236cec7dd56892f3a56
sharmrac <rachit.sharma@hp.com>
2022-08-27 02:11:35 +0530

    Added Service Registry Server

---
+++ user-service/src/test/java/com/rachit/userservice/user/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rachit.userservice.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
